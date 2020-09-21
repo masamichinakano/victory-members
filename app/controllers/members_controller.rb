98194994ceea16134bf2b31e87a47a47afa4732f
masamichinakano <ma5sa9mi0chi9@gmail.com>
2020-09-21 09:34:20 +0900

    バリデーション作成

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -6,4 +6,24 @@ class MembersController < ApplicationController
   def new
     @member = Member.new
   end
+
+  def create
+    # binding.pry
+    @member = Member.new(member_params)
+    if @member.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+
+
+
+  private
+  def member_params
+    params.require(:member).permit(:image, :name, :school_year_id, :age, :tall, :weight,:throwing_id, :hitting_id, :first_position_id, :second_position_id, :third_position_id, :number).merge(user_id: current_user.id)
+  end
+
+
 end
