f59b606c56db322a7af58d3d06ebc08e647c7e27
masamichinakano <ma5sa9mi0chi9@gmail.com>
2020-09-22 10:08:15 +0900

    コントローラー修正

--- app/controllers/defences_controller.rb
+++ app/controllers/defences_controller.rb
@@ -1,6 +1,19 @@
 class DefencesController < ApplicationController
 
   def index
-    
+    @defence = Defence.new
+    @members = Member.all
   end
+
+  def create
+  end
+
+
+
+
+  private
+  def defence_params
+    params.require(:defence).permit(:pitcher_fielder, :catcher_fielder, :first_fielder, :second_fielder, :third_fielder, :short_fielder, :left_fielder, :center_fielder, :right_fielder).merge(member_id: @member.id)
+  end
+
 end
